e6d2a02f0125bed5defee936fb941ea64541fc83
Søren Thomassen <st@randers.dk>
2025-11-04 16:32:15 +0100

    Read txt, text and md files

--- flask/src/utils/extract_filedata.py
+++ flask/src/utils/extract_filedata.py
@@ -12,11 +12,17 @@ def extract_text_from_docx(file):
     return "\n".join([para.text for para in doc.paragraphs])
 
 
+def extract_text_from_txt(file):
+    return file.read().decode('utf-8')
+
+
 def extract_text_from_file(file):
     filename = file.filename.lower()
     if filename.endswith('.pdf'):
         return extract_text_from_pdf(file)
     elif filename.endswith('.docx'):
         return extract_text_from_docx(file)
+    elif filename.endswith('.txt') or filename.endswith('.text') or filename.endswith('.md'):
+        return extract_text_from_txt(file)
     else:
-        raise ValueError("Unsupported file type. Only PDF and DOCX are supported.")
+        raise ValueError("Unsupported file type. Only PDF, DOCX, TXT, and MD are supported.")
