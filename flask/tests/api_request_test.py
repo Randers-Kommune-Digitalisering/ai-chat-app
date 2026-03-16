6fc058ac706cd2b2c3f724ecf784793897332152
Søren Thomassen <st@randers.dk>
2026-03-16 17:19:23 +0100

    Lint

--- flask/tests/api_request_test.py
+++ flask/tests/api_request_test.py
@@ -4,6 +4,7 @@ from unittest.mock import MagicMock, patch
 
 from utils.api_requests import APIClient
 
+
 def test_init():
     api_client = APIClient('http://testurl.com')
 
--- flask/tests/logging_test.py
+++ flask/tests/logging_test.py
@@ -5,6 +5,7 @@ from werkzeug import serving
 
 from utils.logging import disable_endpoint_logs, set_logging_configuration
 
+
 def test_disable_endpoint_logs():
     original_log_request = Mock()
     with patch.object(serving.WSGIRequestHandler, 'log_request', new=original_log_request):
