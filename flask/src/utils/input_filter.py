fff82c75da5c0274d7564013b38709d1852690e9
Nivethan Kandiah <nivethan.kandiah@randers.dk>
2026-05-19 10:50:42 +0200

    added TODO's for input_filter.py

--- flask/src/utils/input_filter.py
+++ flask/src/utils/input_filter.py
@@ -1,6 +1,7 @@
 import re
 
 
+# TODO: add doc string + type hints
 def redact_content(text):
     filtered_content = get_filter_content(text)
     redacted_text = text
@@ -9,12 +10,14 @@ def redact_content(text):
     return redacted_text
 
 
+# TODO: add doc string + type hints
 def get_filter_content(text):
     filtered_content = []
     filtered_content.extend(contains_cpr_number(text))
     return filtered_content
 
 
+# TODO: add doc string + type hints
 def contains_cpr_number(text):
     cpr_pattern = re.compile(r'((((0[1-9]|[12][0-9]|3[01])(0[13578]|10|12)(\d{2}))|(([0][1-9]|[12][0-9]|30)(0[469]|11)(\d{2}))|((0[1-9]|1[0-9]|2[0-8])(02)(\d{2}))|((29)(02)(00))|((29)(02)([2468][048]))|((29)(02)([13579][26])))[-]*\d{4})', re.MULTILINE)
     return [match.group(0) for match in cpr_pattern.finditer(text)]
