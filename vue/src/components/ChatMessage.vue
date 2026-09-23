5f1cdf1be196d74133a584169047ff4434191319
Søren Thomassen <st@randers.dk>
2026-09-23 13:34:33 +0200

    Fix replace HTML when copying response

--- vue/src/components/ChatMessage.vue
+++ vue/src/components/ChatMessage.vue
@@ -73,7 +73,7 @@
 
     const copyTextToClipboard = async (text) => {
         try {
-            const formattedText = text.trim().replace(`<span class="inline-reference">`, '').replace(`</span>`, '')
+            const formattedText = text.trim().replaceAll(`<span class="inline-reference">`, '').replaceAll(`</span>`, '')
             await writeTextToClipboard(formattedText)
             recentlyCopied.value = true
             setTimeout(() => {
